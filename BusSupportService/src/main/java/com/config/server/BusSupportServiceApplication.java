65fd05a59b136b7b4fa8d398c365cba4412160c9
Manonmani@13 <manonmani.pandukumar@gmail.com>
2026-02-05 18:46:59 +0530

    bus

---
+++ BusSupportService/src/main/java/com/config/server/BusSupportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusSupportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusSupportServiceApplication.class, args);
+	}
+
+}
