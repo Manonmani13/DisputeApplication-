de5975e49cf76bf4b9a0163c573d2074c4e063e2
Manonmani@13 <manonmani.pandukumar@gmail.com>
2026-02-11 20:20:21 +0530

    redis

---
+++ RedisEx2/src/main/java/com/config/server/RedisEx2Application.java
@@ -0,0 +1,45 @@
+package com.config.server;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/product")
+public class RedisEx2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisEx2Application.class, args);
+	}
+	  @Autowired
+	    private ProductDao dao;
+
+	    @PostMapping
+	    public Product save(@RequestBody Product product) {
+	        return dao.save(product);
+	    }
+
+	    @GetMapping
+	    public List<Product> getAllProducts() {
+	        return dao.findAll();
+	    }
+
+	    @GetMapping("/{id}")
+	    public Product findProduct(@PathVariable int id) {
+	        return dao.findProductById(id);
+	    }
+	    @DeleteMapping("/{id}")
+	    public String remove(@PathVariable int id)   {
+	    	return dao.deleteProduct(id);
+		}
+}
