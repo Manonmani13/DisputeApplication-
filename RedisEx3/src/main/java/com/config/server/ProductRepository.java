f2a1f90c282355dfe2a67d37c4b0a7c6eb74a46e
Manonmani@13 <manonmani.pandukumar@gmail.com>
2026-02-23 22:14:18 +0530

    Redis

---
+++ RedisEx3/src/main/java/com/config/server/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.config.server;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository
+extends JpaRepository<Product, Long> {
+}
---
+++ RedisEx3/src/main/java/com/config/server/RedisEx3Application.java
@@ -0,0 +1,13 @@
+package com.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisEx3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisEx3Application.class, args);
+	}
+
+}
