545277a5da08d5b9006f4d6ca22b2e0f4461d1c8
Manonmani@13 <manonmani.pandukumar@gmail.com>
2026-03-08 22:12:22 +0530

    authe

---
+++ OAuthExample1/src/main/java/com/example/security/OAuthExample1Application.java
@@ -0,0 +1,13 @@
+package com.example.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthExample1Application.class, args);
+	}
+
+}
