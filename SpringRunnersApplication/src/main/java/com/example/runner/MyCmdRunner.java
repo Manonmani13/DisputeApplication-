ea07477f450d9d18c8ee7bf7ecc2b569369ab9f3
Manonmani@13 <manonmani.pandukumar@gmail.com>
2026-03-24 22:00:03 +0530

    runner

---
+++ SpringRunnersApplication/src/main/java/com/example/runner/MyCmdRunner.java
@@ -0,0 +1,15 @@
+package com.example.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyCmdRunner  implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("MyCmd Runner");
+	}
+
+}
