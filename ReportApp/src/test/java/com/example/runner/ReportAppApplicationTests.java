6eb567c280c82dcaa135be8fa765f17a5f4c32fa
Manonmani@13 <manonmani.pandukumar@gmail.com>
2026-03-27 22:21:47 +0530

    report

---
+++ ReportApp/src/test/java/com/example/runner/ReportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
